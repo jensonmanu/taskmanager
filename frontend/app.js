cbe1e4c9ec7e186061b2126ed74ecf49d352f2ba
jensonmanu <jensonmanu@gmail.com>
2025-11-25 07:19:34 +0530

    app.js updated

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 async function loadTasks() {
-    const res = await fetch("http://your-server-ip/api/tasks");
+    const res = await fetch("http://192.168.1.33/api/tasks");
     const tasks = await res.json();
 
     document.getElementById("taskList").innerHTML = 
@@ -9,7 +9,7 @@ async function loadTasks() {
 async function addTask() {
     const val = document.getElementById("taskInput").value;
 
-    await fetch("http://your-server-ip/api/tasks", {
+    await fetch("http://192.168.1.33/api/tasks", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ task: val })
